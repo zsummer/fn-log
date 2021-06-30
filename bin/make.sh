3db40dce2ddefd4e5f2a7c70dd5d043a1bd2259a
zsummer <yawei.zhang@foxmail.com>
2021-06-30 18:21:09 +0800

    open cmake verbose

--- bin/make.sh
+++ bin/make.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-if [ ! -d build_linux ]; then
-    mkdir build_linux
-fi
-cd build_linux
-if [ $# -gt 0 ] && [ $1 = "max" ]; then
-    echo "has search max version" 
-    if [ -f /usr/bin/g++-8 ]; then 
-        cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-8 -DCMAKE_CXX_COMPILER=/usr/bin/g++-8 $* ../
-    elif [ -f /usr/bin/g++-6 ]; then 
-        cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-6 -DCMAKE_CXX_COMPILER=/usr/bin/g++-6 $* ../
-    else
-        cmake $* ../
-    fi
-else 
-    cmake $* ../
-fi
-make -j2 
+cd ..
+sh make.sh
