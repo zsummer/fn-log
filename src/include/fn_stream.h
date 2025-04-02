db7e71ef6ed5277a7f5abc5b57dc76d60e082d7f
zsummer <yawei.zhang@foxmail.com>
2025-04-02 20:08:31 +0800

    #elif defined(__GCC_ASM_FLAG_OUTPUTS__) && defined(__x86_64__)

--- src/include/fn_stream.h
+++ src/include/fn_stream.h
@@ -87,7 +87,7 @@ namespace FNLog
 #ifdef WIN32
             _mm_lfence();
             return (long long)__rdtsc();
-#else
+#elif defined(__GCC_ASM_FLAG_OUTPUTS__) && defined(__x86_64__)
             unsigned int lo = 0;
             unsigned int hi = 0;
             __asm__ __volatile__("lfence;rdtsc" : "=a" (lo), "=d" (hi) ::);
