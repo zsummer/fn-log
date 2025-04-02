d1cbbe7d1f93532de4c5c7572d5e78481cce1ba6
skylerzhang <skylerzhang@tencent.com>
2025-04-02 22:13:40 +0800

    mac os check_tests.sh

--- check_tests.sh
+++ check_tests.sh
@@ -8,9 +8,13 @@ make -j4
 cd ..
 
 cd bin
-ts=`find ./ -maxdepth 1 -executable -type f  |egrep -v "log" |egrep -v ".gdb*" |egrep -v "*.sh"`
+ts=`find ./ -maxdepth 1  -type f  |egrep -v ".*[a-zA-Z0-9]\..*"`
 for t in $ts ;
 do
+  if [[ "$t" == *test_shm ]]; then
+	echo "jump $t"
+	continue
+  fi
   echo begin test $t 
   $t
   if [ $? -eq 0 ]; then
